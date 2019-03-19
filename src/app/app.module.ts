7576cbd5b38edacc20da37a0e88b4877f6c94a78
Guille <info@guillegonzarod.com>
2019-03-19 21:08:36 +0100

    Almacenando en el Storage del dispositivo - Carrito de Compras.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { IonicStorageModule } from '@ionic/storage';
 
 import { MyApp } from './app.component';
 
@@ -43,7 +44,8 @@ ImagenPipe
   imports: [
     BrowserModule,
     HttpClientModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    IonicStorageModule.forRoot()
   ],
   bootstrap: [IonicApp],
   entryComponents: [
--- src/providers/carrito/carrito.ts
+++ src/providers/carrito/carrito.ts
@@ -1,19 +1,22 @@
 import { Injectable } from '@angular/core';
-import { AlertController } from 'ionic-angular';
+import { AlertController, Platform } from 'ionic-angular';
+import { Storage } from '@ionic/storage';
 
 @Injectable()
 export class CarritoProvider {
 
-  items:any[] = [];
+  items: any[] = [];
 
-  constructor(private alertCtrl: AlertController) {
-    
+  constructor(private alertCtrl: AlertController,
+    private platform: Platform,
+    private storage: Storage) {
+    this.cargar_storage();
   }
 
-  agregar_carrito( item_parametro:any ){
+  agregar_carrito(item_parametro: any) {
 
-    for( let item of this.items ){
-      if( item.codigo == item_parametro.codigo ){
+    for (let item of this.items) {
+      if (item.codigo == item_parametro.codigo) {
 
         this.alertCtrl.create({
           title: "Item existe",
@@ -25,7 +28,56 @@ export class CarritoProvider {
       }
     }
 
-    this.items.push( item_parametro );
+    this.items.push(item_parametro);
+    this.guardar_storage();
+  }
+
+  private guardar_storage() {
+
+    if (this.platform.is("cordova")) {
+      // dispositivo
+      this.storage.set('items', this.items);
+
+    } else {
+      // computadora
+      localStorage.setItem("items", JSON.stringify(this.items));
+
+    }
+  }
+
+  cargar_storage() {
+
+    let promesa = new Promise((resolve, reject) => {
+
+      if (this.platform.is("cordova")) {
+        // dispositivo
+        this.storage.ready()
+          .then(() => {
+
+            this.storage.get("items")
+              .then(items => {
+                if (items) {
+                  this.items = items;
+                }
+                resolve();
+              })
+
+          })
+      } else {
+        // computadora
+        if (localStorage.getItem("items")) {
+          //Existe items en el localstorage
+          this.items = JSON.parse(localStorage.getItem("items"));
+        }
+
+        resolve();
+
+      }
+
+    });
+
+    return promesa;
+
   }
 
 }
